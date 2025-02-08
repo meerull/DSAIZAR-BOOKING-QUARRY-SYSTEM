163436c92ae95ecd58d7a655113729e1cde23019
meerull <2022822454@student.uitm.edu.my>
2025-02-08 15:15:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
                                     {
                                         //Image Available
                                         ?>
-                                        <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" alt="Pizza" class="img-responsive img-curve">
+                                        <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" alt="soil" class="img-responsive img-curve">
                                         <?php
                                     }
                                 ?>
